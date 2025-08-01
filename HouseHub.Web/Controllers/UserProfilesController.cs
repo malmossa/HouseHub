88efe797f7d4e560836b945cfc928a46a2220601
malmossa <malmossa@gmail.com>
2025-08-01 09:12:24 -0700

    completed

---
+++ HouseHub.Web/Controllers/UserProfilesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HouseHub.Shared.Models;
+using HouseHub.Web.Data;
+
+
+
+namespace HouseHub.Web.Controllers
+{
+    public class UserProfilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserProfilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var profiles = await _context.UserProfiles.ToListAsync();
+            return View(profiles);
+        }
+
+        // GET: /UserProfiles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /UserProfiles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                userProfile.Id = Guid.NewGuid();
+                userProfile.CreatedAt = DateTime.UtcNow;
+                _context.Add(userProfile);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Delete/{id}
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var profile = await _context.UserProfiles.FindAsync(id);
+
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return View(profile);
+        }
+
+        // POST: UserProfiles/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var profile = await _context.UserProfiles.FindAsync(id);
+
+            if (profile != null)
+            {
+                _context.UserProfiles.Remove(profile);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
